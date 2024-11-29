9e17921d4ff34ee67fd657fa171e0ceb578a0e47
lucas <lucaspish@gmail.com>
2024-11-29 20:44:48 -0300

    mais algumas modificações no back

--- src/components/layout/SendService.tsx
+++ src/components/layout/SendService.tsx
@@ -1,20 +1,33 @@
-import {database, ref, set} from '../firebase/Firebase'
+import {database, ref, set, auth, get} from '../firebase/Firebase'
 
 export default async function SendService(data:any, navigation:any){
-    const databaseRef = ref(database, 'services/');
-    const serviceData ={
-        endereco: data.adress,
-        preco: data.price,
-        tipoAnimal: data.animalType,
-        qtdService: data.qtdService
-    }
-
-    set(databaseRef, serviceData)
-    .then(()=>{
-        alert("serviço criado com sucesso");
-        navigation('/Login')
+    const user = auth.currentUser
+    const databaseRef = ref(database, 'services/'+user?.uid);
+    const dbUserRef = ref(database, 'users/'+user?.uid+'/nome')
+    get(dbUserRef).then((snapshot)=>{
+        if (snapshot.exists()) {
+            const nomep = snapshot.val(); // Recupera o valor do campo "nome"
+            const serviceData ={
+                endereco: data.adress,
+                preco: data.price,
+                tipoAnimal: data.animalType,
+                qtdService: data.qtdService,
+                userUid: user?.uid,
+                nomePrestador: nomep
+            }
+            set(databaseRef, serviceData)
+            .then(()=>{
+                alert("serviço criado com sucesso");
+                navigation('/MenuPrestador')
+            })
+            .catch((error:any)=>{
+                alert(error.code);
+            })
+        } else {
+            console.log("Nenhum dado encontrado.");
+        }
     })
-    .catch((error:any)=>{
-        alert(error.code);
-    })
+    .catch((error) => {
+        console.error("Erro ao acessar o banco de dados:", error);
+    });
 }
