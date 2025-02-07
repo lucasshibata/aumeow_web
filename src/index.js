3cea82e206dde33145efea2c3cc09c42c94a1aae
lucas <lucaspish@gmail.com>
2025-02-07 14:56:12 -0300

    implementação bem sucedida do PWA

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './index.css'
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { register } from './serviceWorkerRegistration';
 
 const container = document.getElementById("root")
 const root = ReactDOM.createRoot(container)
 root.render(<App/>)
+register();
