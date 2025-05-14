49e02761f83bc0b58bf4ba37237e0588887cde97
Lucas Shibata <lucaspish@gmail.com>
2025-05-14 09:49:06 -0300

    edições inciais da página de adoção

--- src/components/screens/CadastroDeAdocao/CadastroDeAdocao.tsx
+++ src/components/screens/CadastroDeAdocao/CadastroDeAdocao.tsx
@@ -1,7 +1,217 @@
+import { useState, useRef, useEffect } from "react";
+import { ref, database, auth, get, set} from "../../firebase/Firebase";
+import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import verifyFunction from "../../layout/verifyFunction";
+import Header from "../../layout/Header";
+import Footer from "../../layout/Footer";
+
+interface AdocaoData {
+    Nome: string;
+    Especie: string;
+    Raca: string;
+    Sexo:string;
+    Idade:number;
+    Porte:string;
+    EstadoDeSaude: string;
+    Temperamento: string;
+    HistoriaDoAnimal: string;
+    NomeResponsavel: string;
+    UIDResponsavel: string;
+    EmailResponsavel: string; 
+}
+
 export default function CadastroDeAdocao(){
+    const { register, handleSubmit } = useForm();
+    const navigate = useNavigate();
+    const [file, setFile] = useState<File | null>(null);
+    const [uploading, setUploading] = useState(false);
+    const [imagePreview, setImagePreview] = useState<string | null>(null);
+    const [loading, setLoading] = useState(true);
+    const fileInputRef = useRef<HTMLInputElement>(null);;
+    
+    const handleFileChange = (e:any) => {
+        if (e.target.files && e.target.files[0]) {
+          setFile(e.target.files[0]);
+          const imageUrl = URL.createObjectURL(e.target.files[0]);
+          setImagePreview(imageUrl);
+        }
+    };
+
+    const handleClick = () => {
+        if (fileInputRef.current) {
+            fileInputRef.current.click();
+          } // abre o seletor de arquivos
+    };
+
+     useEffect(()=>{
+        const verificar = async () => {
+            const funcao = await verifyFunction();
+            
+            switch (funcao){
+                case "prestador":
+                    navigate("/MenuPrestador");
+                    break;
+                case "cliente":
+                    navigate("/MenuCliente");
+                    break;
+                default:
+                    console.log("permitido ou não encontrado");
+            }
+            setLoading(false);
+        }
+        verificar();
+    },[navigate])
+
+    if(loading){
+        <div>loading...</div>
+    }
+
+    const onSubmit = async (data: any) => {
+        if (!file) {
+        alert('Por favor, selecione uma imagem.');
+        return;
+        }
+
+        setUploading(true);
+
+        const user = auth.currentUser;
+        const productRef = ref(database, 'adocao/' + data.code); //resolver codigo que vai ser usado como referência
+        const dbUserNameRef = ref(database, 'users/' + user?.uid + '/nome');
+        const dbUserEmailRef = ref(database, 'users/' + user?.uid + '/email');
+
+        // Declare productData com a interface ProductData
+        let AdocaoData: AdocaoData = {
+            Nome: data.nome,
+            Especie: data.especie,
+            Raca: data.raca,
+            Sexo:data.sexo,
+            Idade:data.idade,
+            Porte:data.porte,
+            EstadoDeSaude: data.estadoDeSaude,
+            Temperamento: data.temperamento,
+            HistoriaDoAnimal: data.historiaDoAnimal,
+            NomeResponsavel: '',
+            UIDResponsavel: user?.uid || '',
+            EmailResponsavel: data.email
+        };
+
+        try {
+        // Obtém o nome do usuário
+        const userNameSnapshot = await get(dbUserNameRef);
+        if (userNameSnapshot.exists()) {
+            AdocaoData.NomeResponsavel = userNameSnapshot.val();
+        }
+
+        // Obtém o email do usuário
+        const userEmailSnapshot = await get(dbUserEmailRef);
+        if (userEmailSnapshot.exists()) {
+            AdocaoData.EmailResponsavel = userEmailSnapshot.val(); // Agora é seguro adicionar
+        }
+
+        // Verifica se o produto já existe
+        const productSnapshot = await get(productRef);
+        if (productSnapshot.exists()) {
+            alert(`O código "${data.code}" já existe. Escolha outro.`);
+            return;
+        }
+
+        // Salva os dados do produto no Firebase
+        await set(productRef, AdocaoData);
+
+        // Faz o upload da imagem para o S3
+        // await s3.upload({
+        //     Bucket: 'aumeow-images',
+        //     Key: `${data.code}/imagemProduto`,
+        //     Body: file,
+        //     ContentType: file.type,
+        // }).promise();
+
+        alert('Produto criado com sucesso e salvo no banco!');
+        console.log('Arquivo enviado com sucesso');
+
+        // Navega para a tela do menu do prestador
+        navigate('/MenuPrestador');
+        } catch (error) {
+        console.error('Erro ao adicionar o produto:', error);
+        alert('Erro ao adicionar o produto. Tente novamente.');
+        } finally {
+        setUploading(false);
+        }
+    };
+
     return(
         <div className="ContainerCadastroDeAdocao">
-            olá
+            <Header/>
+            <div className='InnercontainerCadastroDeAdocao'>
+                <h1 className='TitleCadastroDeAdocao'>Cadastro de Produtos</h1>
+                <form className='FormContainerCadastroDeAdocao' onSubmit={handleSubmit(onSubmit)}>
+                    {imagePreview && (
+                        <div  style={{ display:'flex', flexDirection:'column', width: '100%', height: 'auto', alignItems:'center'}}>
+                            <h3 className='txtCadastroDeAdocao'>Prévia da Imagem:</h3>
+                            <img
+                                src={imagePreview}
+                                alt="Prévia da imagem selecionada"
+                                style={{ width: '40%', height: '100%', marginTop: '10px' }}
+                            />
+                        </div>
+                    )}
+                    <label className='txtCadastroDeAdocao'>Selecione uma imagem:</label>
+                    <input
+                        type="file"
+                        accept="image/*"
+                        onChange={handleFileChange}
+                        ref={fileInputRef}
+                        style={{ display: 'none' }}
+                    />
+                    <button
+                        onClick={handleClick}
+                        className="botaoUploadImagemCadastroDeAdocao"
+                    >Selecionar imagem</button>
+                    <label className='txtCadastroDeAdocao'>Nome do produto:</label>
+                    <input
+                        className='InputTextCadastroDeAdocao'
+                        type='text'
+                        placeholder='Nome:'
+                        {...register("name", { required: true })}
+                    />
+                    <label className='txtCadastroDeAdocao'>Código do produto:</label>
+                    <input
+                        className='InputTextCadastroDeAdocao'
+                        type='text'
+                        placeholder='Código:'
+                        {...register("code", { required: true })}
+                    />
+                    <label className='txtCadastroDeAdocao'>Quantidade em estoque:</label>
+                    <input
+                        className='InputTextCadastroDeAdocao'
+                        type='text'
+                        placeholder='Quantidade:'
+                        {...register("amount", { required: true })}
+                    />
+                    <label className='txtCadastroDeAdocao'>Preço do produto:</label>
+                    <input
+                        className='InputTextCadastroDeAdocao'
+                        type='text'
+                        placeholder='Preço:'
+                        {...register("price", { required: true })}
+                    />
+                    <label className='txtCadastroDeAdocao'>Marca do produto:</label>
+                    <input
+                        className='InputTextCadastroDeAdocao'
+                        type='text'
+                        placeholder='Marca:'
+                        {...register("brand", { required: true })}
+                    />
+                    <input
+                        className='SubmitCadastroDeAdocao'
+                        value={uploading ? 'Enviando...' : 'Enviar'}
+                        type="submit"
+                        disabled={uploading}
+                    />
+                </form>
+            </div>
+            <Footer/>
         </div>
     )
 }
--- src/components/screens/SobreNos/SobreNos.tsx
+++ src/components/screens/SobreNos/SobreNos.tsx
@@ -9,13 +9,13 @@ export default function SobreNos(){
             <Header/>
             <div className="InnercontainerSobreNos">
                 <article className="Article1SobreNos">
-                    <img src={navImg4} alt="" />
+                    <img className="Img1SobreNos" src={navImg4} alt="" />
                     <p>Quem está por trás do AuMeow?</p>
-                    <img src={navImg4} alt="" />
+                    <img className="Img2SobreNos" src={navImg4} alt="" />
                 </article>
                 <article className="Article2SobreNos">
                     <div className="DivInterna1SobreNos">
-                        <img src={navImg4} alt="" />
+                        <img className="Img3SobreNos" src={navImg4} alt="" />
                         <p> Somos um grupo de desenvolvedores e alunos do curso de Ciências da Computação, do CEUB, nosso principal objetivo com esta aplicação Web/Aplicativo 
                             é nos capacitar no desenvolvimento Web e Mobile com a utilização de ferramentas como Figma, Trello, HTML/CSS, React Web, Firebase, nuvem da 
                             AWS(Amazon Web Services).
@@ -25,10 +25,10 @@ export default function SobreNos(){
                         <p> Nossa equipe preza pelo bem dos animais, e com isso surge a ideia deste projeto, com o proposito de unificar todas as necessidades de um dono de pets 
                             em um único lugar, permitindo auxiliar nas buscas de serviços, produtos e auxilio profissional, além de auxiliar no controle dos cuidados de seus animais, 
                             como consultas veterinárias, agendamento de vacinação, banho e tosa, serviços de passeio, sistema de adoção e um canal para denuncias.</p>
-                        <img src={navImg4} alt="" />
+                        <img className="Img4SobreNos" src={navImg4} alt="" />
                     </div>
                 </article>
-                <button>Saiba mais sobre nossas redes sociais</button>
+                <button className="Btn1SobreNos">Saiba mais sobre nossas redes sociais</button>
             </div>
             <Footer/>
         </div>
